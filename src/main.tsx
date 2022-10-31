52148fe59162d33f1472734fb266b96f44e99b72
Josh Salsbury <joshuasalsbury@gmail.com>
2022-10-31 00:10:15 -0700

    Added firebase to project

--- src/main.tsx
+++ src/main.tsx
@@ -11,10 +11,36 @@ import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 
 // Theme
-// Theme
 import { createTheme, responsiveFontSizes } from '@mui/material/styles';
 import { ThemeProvider } from '@emotion/react'
 import { CssBaseline } from '@mui/material'
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app"
+import { getAnalytics } from "firebase/analytics"
+import { getAuth } from 'firebase/auth'
+import { getFirestore } from 'firebase/firestore'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCoo8oAAEs6rswUoT8YEi-3sHYwsRkIkBo",
+  authDomain: "inclusive-books.firebaseapp.com",
+  projectId: "inclusive-books",
+  storageBucket: "inclusive-books.appspot.com",
+  messagingSenderId: "431033050380",
+  appId: "1:431033050380:web:24597e1aac6ff5a0ced529",
+  measurementId: "G-YNXEFTM5Z2"
+}
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
+const analytics = getAnalytics(app)
+const auth = getAuth(app)
+const db = getFirestore(app)
+
 
 let theme = createTheme({
   palette: {
