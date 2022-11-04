11366dce509d627d4a8ca44f12ff9a31d08363e9
Josh Salsbury <joshuasalsbury@gmail.com>
2022-11-04 11:56:09 -0700

    Updated Nav component for simplified Navbar.
    Created UserMenu component in Nav with drop down menu. Defaults to Sign In button if user is not signed in.

--- src/components/Navigation/Nav.tsx
+++ src/components/Navigation/Nav.tsx
@@ -3,24 +3,55 @@ import React from 'react'
 // Components
 import AppBar from '@mui/material/AppBar'
 import Toolbar from '@mui/material/Toolbar'
-import NavMenu from './NavMenu'
 
 // Utils
 import { MenuPath } from '../../utils/menuPaths'
+import { Avatar, Button, Typography } from '@mui/material'
+import { NavLink } from 'react-router-dom'
+import { UserAuth } from '../../contexts/AuthContext'
+import UserMenu from './UserMenu'
 
 
 export default function Nav(props : { menuPaths: MenuPath[] }) {
+
+    const {googleSignIn, user} = UserAuth()
+
+    const activeStyle = { color: '#6c5ce7', textDecoration: 'underline', textUnderlineOffset: '10px', textDecorationThickness: '2px' }
+    const inactiveStyle = { color: 'black' }
+
     return (
         <AppBar
             position='fixed'
             className='nav-bar'
+            sx={{ backgroundColor: 'white', boxShadow: 'none', borderBottom: 'solid 1px #b2bec3', px: 2 }}
         >
-            <Toolbar            >
+            <Toolbar sx={{ display: 'flex', justifyContent: 'flex-start', columnGap: 5 }}>
 
-                <div>Navbar Component</div>
-                <div>
-                    <NavMenu menuPaths={props.menuPaths} />
-                </div>
+                <Typography color='black'>
+                    SMART Reading Book Search
+                </Typography>
+
+                <NavLink to='/' style={({isActive}) => isActive ? activeStyle : inactiveStyle}>
+                    Home
+                </NavLink>
+
+                <NavLink to='/books/quick-search' style={({isActive}) => isActive ? activeStyle : inactiveStyle}>
+                    Quick Search
+                </NavLink>
+                
+                <NavLink to='/books/advanced-search' style={({isActive}) => isActive ? activeStyle : inactiveStyle}>
+                    Advanced Search
+                </NavLink>
+                
+                {
+                    (user?.displayName) ? 
+                    
+                        <UserMenu user={user} /> : 
+                    
+                        <Button variant='outlined' sx={{ marginLeft: 'auto' }} onClick={googleSignIn}>
+                            Sign In
+                        </Button>
+                }
 
             </Toolbar>
         </AppBar>
