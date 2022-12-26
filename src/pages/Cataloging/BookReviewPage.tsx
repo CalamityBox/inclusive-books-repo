c280590737983823036f64309e20495f5fe700e4
Josh Salsbury <joshuasalsbury@gmail.com>
2022-12-25 16:11:59 -0800

    Added user data to review submission in Cataloging review.

--- src/pages/Cataloging/BookReviewPage.tsx
+++ src/pages/Cataloging/BookReviewPage.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
 import ReviewForm from '../../components/Forms/ReviewForm'
+import { UserAuth } from '../../contexts/AuthContext'
 import { submitReview } from '../../utils/useDatabase'
 
 export default function BookReviewPage() {
@@ -8,11 +9,17 @@ export default function BookReviewPage() {
     const { id } = useParams()
     console.log('book id is:',id)
 
+    const { user } = UserAuth()
+
     return (
         <>
             Book Review Page
             <ReviewForm 
                 handleSubmit={(data: any) => {
+                    data.reviewer = {
+                        name: user.displayName,
+                        id: user.uid
+                    }
                     console.log('submitting data',data)
                     !!id ? submitReview(id, data) : console.log('id undefined')
                 }} 
