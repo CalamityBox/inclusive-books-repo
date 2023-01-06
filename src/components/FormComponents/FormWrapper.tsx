5a50f3cb2dbb606db466d9bfbb5cebe0a271db94
Josh Salsbury <joshuasalsbury@gmail.com>
2023-01-06 12:19:33 -0800

    Added canEdit property to determine on ui whether or not user can edit and submit form. Note: this is not the same as the can read or write functionality on firebase. Rather, it is feedback to help the user. Need to add functionality to disable form fields in read-only mode.

--- src/components/FormComponents/FormWrapper.tsx
+++ src/components/FormComponents/FormWrapper.tsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 import { yupResolver } from '@hookform/resolvers/yup'
 import { FormProvider, SubmitHandler, useForm } from 'react-hook-form'
-import { Button, Container } from '@mui/material'
+import { Alert, Button, Container } from '@mui/material'
 
-export default function FormWrapper(props: { defaultValues: any, schema: any, children: any, formSubmitHandler?: any, handleBlur?: SubmitHandler<any> }) {
+export default function FormWrapper(props: { defaultValues: any, schema: any, children: any, formSubmitHandler?: any, handleBlur?: SubmitHandler<any>, canEdit?: boolean }) {
 
     // Not using form wrapper to leave methods exposed; need to use getValue to submit form data when step changes
     const methods = useForm({
@@ -17,6 +17,8 @@ export default function FormWrapper(props: { defaultValues: any, schema: any, ch
         console.log('form data:',data)
     }
 
+    const canEdit = props?.canEdit === undefined ? true : props.canEdit // Set default value of undefined to true because that is the more common use case
+
     const handleSubmit = !!props?.formSubmitHandler ? props.formSubmitHandler : defaultFormSubmitHandler
 
     return (
@@ -26,8 +28,23 @@ export default function FormWrapper(props: { defaultValues: any, schema: any, ch
                 onBlur={methods.handleSubmit(!!props?.handleBlur ? props.handleBlur : () => {})}
             >
                 <Container maxWidth={false} sx={{ display: 'flex', flexDirection: 'column', rowGap: 3, maxWidth: 900 }}>
+                    {
+                        canEdit ?
+                            <></>
+                            :
+                            <Alert severity='info'>
+                                You are viewing this form in read-only mode because you do not have permission to edit.
+                            </Alert>
+                    }
                     {props.children}
-                    <Button type='submit' variant='contained' sx={{ margin: 'auto' }}>Submit</Button>
+                    {
+                        canEdit ?
+                            <Button type='submit' variant='contained' sx={{ margin: 'auto' }}>
+                                Submit
+                            </Button>
+                            :
+                            <></>
+                    }
                 </Container>
             </form>
         </FormProvider>
